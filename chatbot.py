44dad716d61b3815a4b50e9b9e38950b439ee356
Srinivasa V <srini.v.telkar@gmail.com>
2020-05-31 19:05:59 +0530

    Added the inital chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('Bot')
+bot.set_trainer(ListTrainer)
+
+for files in os.listdir('C:/Users/Windows 8.1/AppData/Local/Programs/Python/Python35/Lib/site-packages\chatterbot_corpus/data/english/'):
+	data = open('C:/Users/Windows 8.1/AppData/Local/Programs/Python/Python35/Lib/site-packages\chatterbot_corpus/data/english/'+files,'r').readlines()
+	bot.train(data)
+
+while True:
+	message = input('U:')
+	if message.strip() != 'Bye':
+		reply = bot.get_response(message)
+		print('ChatBot :',reply)
+	if message.strip() == 'Bye':
+		print('ChatBot : Bye')
+		break
